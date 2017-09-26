47fa26828d21ba3e88df8061ea649272c2c6d58b
Natalia <natalia.kielbicka@o2.pl>
2017-09-26 15:27:24 +0200

    add submit btn to search and correct logo displaying on mobile

--- src/components/views/header/SearchAll.jsx
+++ src/components/views/header/SearchAll.jsx
@@ -5,6 +5,7 @@ export default class SearchAll extends Component {
     return (
       <form className="form-inline float-left">
         <input className="form-control mr-sm-2" placeholder="Search" type="text"/>
+        <button type="submit" value="search" className="search-btn"><img src="img/search-icon.png" alt=""/></button>
       </form>
     )
   }
