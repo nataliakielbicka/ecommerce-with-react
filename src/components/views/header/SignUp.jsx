b85784fe0bd1486436770a221f169a8dcd0540c4
Natalia <natalia.kielbicka@o2.pl>
2017-09-26 20:53:06 +0200

    create signUpModal component

--- src/components/views/header/SignUp.jsx
+++ src/components/views/header/SignUp.jsx
@@ -1,9 +1,27 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import SignUpModal from './SignUpModal';
 
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.toggleModal = this
+      .toggleModal
+      .bind(this);
+  }
+  toggleModal() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <a href="#" className="btn btn-blue float-right">Sign Up</a>
+      <div>
+        <button className="btn btn-blue float-right" onClick={this.toggleModal}>Sign Up</button>
+        <SignUpModal show={this.state.isOpen} onClose={this.toggleModal}/>
+      </div>
     )
   }
 }
---
+++ src/components/views/header/SignUpModal.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+
+export default class SignUpModal extends Component {
+    render() {
+        if (!this.props.show) {
+            return null;
+        }
+        const displayBlock = this.props.show
+            ? "show d-block"
+            : ""
+
+        return (
+            <div>
+                <div className={`modal fade ${displayBlock}`}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+
+                                <h4 className="modal-title">Modal title</h4>
+                                <button
+                                    type="button"
+                                    className="close"
+                                    aria-hidden="true"
+                                    onClick={this.props.onClose}>×</button>
+                            </div>
+                            <div className="modal-body">
+                                <p>One fine body…</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-yellow" onClick={this.props.onClose}>Cancel</button>
+                                <button type="submit" className="btn btn-blue" onClick={this.props.onClose}>Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={`modal-backdrop fade ${displayBlock}`}></div>
+            </div>
+        )
+    }
+}
